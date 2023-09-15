7112045da06f2ddceb9339c4e232a1206d749513
noahkohrs <noah@kohrs.fr>
2023-09-15 09:41:46 +0200

    added boost view2

--- videoconverter.py
+++ videoconverter.py
@@ -9,6 +9,7 @@ def resize_video(filename, arival = 'new_short.mp4') -> str:
     desired_height = 1280
     desired_width = int(desired_height * 9 / 16)
     black_bar_height = 200
+    boost_size = 180
 
     # Redimensionnez d'abord la hauteur du clip à la hauteur souhaitée
     clip_resized = clip.resize(height=desired_height - black_bar_height*2)
@@ -20,8 +21,17 @@ def resize_video(filename, arival = 'new_short.mp4') -> str:
     # Ajoutez des barres noires en haut et en bas pour obtenir un rapport hauteur / largeur de 16: 9
     padded_clip = cropped_clip.fx(fx.all.margin, top=black_bar_height, bottom=black_bar_height, left=0, right=0)
 
-    final_clip = CompositeVideoClip([padded_clip])
-    # Render on graphics card with a video codec that is not libx264
+
+    boost = clip.fx(fx.all.crop, x1=clip.w-295, y1=clip.h-295 ,x2=clip.w-40, y2=clip.h-40)
+    boost = boost.resize(height=boost_size)
+    boost = boost.set_position(((desired_width-boost_size)/desired_width, (desired_height-boost_size-black_bar_height)/desired_height), relative=True)
+    boost = boost.margin(right=0, top=0)
+
+    boost = boost.set_duration(clip.duration-4)
+    boost = boost.set_opacity(0.85)
+    
+    final_clip = CompositeVideoClip([padded_clip, boost])
+
     final_clip.write_videofile(arival, codec='libx264', remove_temp=True, audio_codec='aac', threads=4, bitrate='5000k')
 
     
