72c6bce9dd359eb680eb319bdefe4b5d3898886f
noahkohrs <86845087+noahkohrs@users.noreply.github.com>
2023-11-07 20:24:19 +0100

    Removing useless comments.

--- videoconverter.py
+++ videoconverter.py
@@ -3,25 +3,23 @@ from moviepy.video import fx
 
 def resize_video(filename, arival = 'new_short.mp4') -> str:
     clip = VideoFileClip(filename)
-    
-    # Déterminez la hauteur et la largeur souhaitées
 
     desired_height = 1280
     desired_width = int(desired_height * 9 / 16)
     black_bar_height = 200
     boost_size = 180
 
-    # Redimensionnez d'abord la hauteur du clip à la hauteur souhaitée
+    # Resize to the wanted height.
     clip_resized = clip.resize(height=desired_height - black_bar_height*2)
 
 
-    # Sinon, coupez les parties excédentaires de la vidéo
+    # Cutout overflowing parts of the video.
     cropped_clip = clip_resized.crop(x_center=clip_resized.w/2, width=desired_width)
 
-    # Ajoutez des barres noires en haut et en bas pour obtenir un rapport hauteur / largeur de 16: 9
+    # Adding black bars.
     padded_clip = cropped_clip.fx(fx.all.margin, top=black_bar_height, bottom=black_bar_height, left=0, right=0)
 
-
+    # Showing the boost left.
     boost = clip.fx(fx.all.crop, x1=clip.w-295, y1=clip.h-295 ,x2=clip.w-40, y2=clip.h-40)
     boost = boost.resize(height=boost_size)
     boost = boost.set_position(((desired_width-boost_size)/desired_width, (desired_height-boost_size-black_bar_height)/desired_height), relative=True)
